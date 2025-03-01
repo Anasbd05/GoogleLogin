058f54002c4f909836a39a0a7fb06f6a214f0737
anas bd <anastrying05@gmail.com>
2025-03-01 18:13:09 +0000

    add news

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,38 @@
 "use client"
 import {supabase} from '@/lib/supabaseClient'
+import {useState} from 'react'
 
 const Login = () => {
+
+    const [Email,setEmail] = useState('')
+
     const loginWithGoogle = async () => {
         supabase.auth.signInWithOAuth({
             provider: "google"
         })
     }
 
+    const loginWithEmail = async () => {
+        supabase.auth.signInWithOtp({
+            email: Email
+        })
+    }
+
+
+
 
 
     return (
         <div className='flex items-center h-screen justify-center'>
-            <div className="w-3/4 md:w-2/4 flex flex-col items-center border border-gray-200 p-6 shadow-md">
+            <div className="w-3/4 md:w-2/4 mx-auto flex flex-col items-center border border-gray-200 p-6 shadow-md">
                 <h1 className='text-4xl text-center font-bold'>Login</h1>
                 <button onClick={loginWithGoogle} className='my-4 bg-sky-300 py-2 px-6 hover:opacity-80'>LOGIN WITH GOOGLE</button>
+                <div>
+                    <input value={Email} onChange={(e) => setEmail(e.target.value)} type="email" className='py-2 bg-gray-200 outline-0 px-4' placeholder='email' />
+                    <button onClick={loginWithEmail} className='my-4 bg-sky-300 py-2 px-6 hover:opacity-80'>LOGIN WITH EMAIL</button>
+                </div>
             </div>
+
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
+"use client"
+import {supabase} from '@/lib/supabaseClient';
 import Login from './login/page'
+import React,{useEffect,useState} from 'react'
 
 const Home = () => {
+  const handleupload = async (e: ChangeEvent<HTMLInputElement>) => {
+    let file;
+    if(e.target.files) {
+      file = e.target.files[0]
+    }
+    const {data,error} = await supabase.storage.from('images')
+      .upload("public/" + file.name,file as File)
+
+    if(data) {
+      console.log(data)
+    } else if(error) {
+      console.log(error)
+    }
+  }
+  const [user,setUser] = useState(null)
+
+  const GetUser = async () => {
+    const {data,error} = await supabase.auth.getSession()
+    if(error) {
+      console.log(error)
+    } else if(!data) {
+      console.log("no data")
+    } else {
+      setUser(data.session)
+    }
+  }
+
+  useEffect(() => {
+    GetUser()
+  },[]) // Empty dependency array ensures it runs only once on mount
+
+  console.log(user)
+
   return (
-    <div>
+    <div className='flex my-10 flex-col items-center w-full justify-center h-screen'>
+      <input onChange={(e) => handleupload(e)} type="file" accept='image/*'
+        className='py-2 px-6 bg-gray-200 border-2 dark:bg-gray-800 rounded-full text-gray-800 ' id="file-image" />
+      {/* <img src="https://ywsfekncwsrkyajnhcww.supabase.co/storage/v1/object/public/images/public/ScreenShot%20Tool%20-20250226062509.png" alt="" /> */}
       <Login />
+
     </div>
   )
 }
